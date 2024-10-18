16737d43457673143916cdb74edf01c598f45c9f
Gagan Ahuja <gdsa006@gmail.com>
2024-10-18 20:27:10 +0530

    new

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -8,35 +8,35 @@ import TestimonialCard from '../partials/TestimonialCard';
 const testimonials = [
   {
     id: 1,
-    image: "../images/testimonial1.webp",
+    image: "../images/Asphalt1.webp",
     text: "They were incredibly friendly and fast. And they did an amazing",
     author: "Ashley Cliff",
     rating: "5"
   },
   {
     id: 2,
-    image: "./images/testimonial2.webp",
+    image: "./images/Asphalt2.webp",
     text: "The owner of the company is absolutely AMAZING! The customer is their #1 priority and providing you with the best possible product they can.",
     author: "Jane Smith",
     rating: "5"
   },
   {
     id: 3,
-    image: "./images/testimonial3.webp",
+    image: "./images/Asphalt3.webp",
     text: "I had a unique situation and laid it out to them and without hesitation they jumped through the various hoops to make me as happy as possible. Great company!",
     author: "Amanda Canoy",
     rating: "5"
   },
   {
     id: 4,
-    image: "./images/testimonial4.webp",
+    image: "./images/Asphalt4.webp",
     text: "Great installation crew was at my house at 7:30AM and the roof was complete by 2PM. Good coordination of supplies, labor and cleanup!",
     author: "Derek Lemon",
     rating: "5"
   },
   {
     id: 5,
-    image: "./images/testimonial5.webp",
+    image: "./images/Asphalt5.webp",
     text: "These guys worked with us and were super easy going. Augustine and his crew went above and beyond. He is a man of his word. Our roof looks INCREDIBLE THANK YOU!",
     author: "Wayne & Tami",
     rating: "5"
